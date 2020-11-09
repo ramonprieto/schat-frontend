9e1bc5b1e43dbb89e5d4afa399be12610497d764
Ramon Prieto <ramonprieto12@gmail.com>
2020-11-09 09:02:11 -0700

    Skeleton project

---
+++ src/components/Chat.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 offset-3">
+
+        <div v-if="sessionStarted" id="chat-container" class="card">
+          <div class="card-header text-white text-center font-weight-bold subtle-blue-gradient">
+            Simplicity Chat
+          </div>
+
+          <div class="card-body">
+            <div class="container chat-body">
+              <div v-for="message in messages" :key="message.id" class="row chat-section">
+                <template v-if="username === message.user.username">
+                  <div class="col-sm-7 offset-3">
+                    <span class="card-text speech-bubble speech-bubble-user float-right text-white subtle-blue-gradient">
+                      {{ message.message }}
+                    </span>
+                  </div>
+                  <div class="col-sm-2">
+                    <img class="rounded-circle" :src="`http://placehold.it/40/007bff/fff&text=${message.user.username[0].toUpperCase()}`" />
+                  </div>
+                </template>
+                <template v-else>
+                  <div class="col-sm-2">
+                    <img class="rounded-circle" :src="`http://placehold.it/40/333333/fff&text=${message.user.username[0].toUpperCase()}`" />
+                  </div>
+                  <div class="col-sm-7">
+                    <span class="card-text speech-bubble speech-bubble-peer">
+                      {{ message.message }}
+                    </span>
+                  </div>
+                </template>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-footer text-muted">
+            <form @submit.prevent="postMessage">
+              <div class="row">
+                <div class="col-sm-10">
+                  <input v-model="message" type="text" placeholder="Type a message" />
+                </div>
+                <div class="col-sm-2">
+                  <button class="btn btn-primary">Send</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div v-else>
+          <h3 class="text-center">Welcome !</h3>
+          <br />
+          <p class="text-center">
+            To start chatting with friends click on the button below, it'll start a new chat session
+            and then you can invite your friends over to chat!
+          </p>
+          <br />
+          <button @click="startChatSession" class="btn btn-primary btn-lg btn-block">Start Chatting</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+const $ = window.jQuery
+
+export default {
+  data () {
+    return {
+      sessionStarted: false,
+      messages: [],
+      message: ''
+    }
+  },
+
+  created () {
+    this.username = sessionStorage.getItem('username')
+
+    // Setup headers for all requests
+    $.ajaxSetup({
+      headers: {
+        'Authorization': `Token ${sessionStorage.getItem('authToken')}`
+      }
+    });
+
+    if (this.$route.params.uri) {
+      this.joinChatSession();
+    };
+
+    this.connectToWebSocket();
+  },
+
+  methods: {
+    startChatSession () {
+      $.post('http://localhost:8000/api/chats/', (data) => {
+        alert("A new session has been created you'll be redirected automatically")
+        this.sessionStarted = true
+        this.$router.push(`/chats/${data.uri}/`)
+      })
+      .fail((response) => {
+        alert(response.responseText)
+      })
+    },
+
+    postMessage(event) {
+      const data = {
+        message: this.message,
+        username: this.username,
+        uri: this.$route.params.uri
+      };
+      this.chatSocket.send(JSON.stringify(data));
+      this.message = ''
+    },
+
+    joinChatSession () {
+      const uri = this.$route.params.uri
+
+      $.ajax({
+        url: `http://localhost:8000/api/chats/${uri}/`,
+        data: {username: this.username},
+        type: 'PATCH',
+        success: (data) => {
+          const user = data.members.find((member) => member.username === this.username)
+
+          if (user) {
+            // The user is part of the session
+            this.sessionStarted = true
+            this.fetchChatSessionHistory()
+          }
+        }
+      })
+    },
+
+    fetchChatSessionHistory () {
+      $.get(`http://localhost:8000/api/chats/${this.$route.params.uri}/messages/`, (data) => {
+        this.messages = data.messages
+      })
+    },
+
+    connectToWebSocket() {
+      const roomName = this.$route.params.uri
+      console.log('Attempting to connect to websocket at .','ws://127.0.0.1:8000/ws/chat/' + roomName + '/');
+      this.chatSocket = new ReconnectingWebSocket(
+        'ws://127.0.0.1:8000/ws/chat/' + roomName + '/'
+      );
+      console.log('Successfully connected to websocket.');
+      this.chatSocket.onopen = this.onOpen;
+      this.chatSocket.onclose = this.onClose;
+      this.chatSocket.onmessage = this.onMessage;
+      this.chatSocket.onerror = this.onError;
+    },
+
+    onOpen(event) {
+      console.log('Connectioned opened.', event.data);
+    },
+
+    onClose(event) {
+      console.log('Connection closed.', event.data);
+    },
+
+    onMessage(event) {
+      console.log('Sending message through websocket.');
+      const message = JSON.parse(event.data);
+      this.messages.push(message);
+    },
+
+    onError(event) {
+      alert('An error ocurred: ', event.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.btn {
+  border-radius: 0 !important;
+}
+
+.card-footer input[type="text"] {
+  background-color: #ffffff;
+  color: #444444;
+  padding: 7px;
+  font-size: 13px;
+  border: 2px solid #cccccc;
+  width: 100%;
+  height: 38px;
+}
+
+.card-header a {
+  text-decoration: underline;
+}
+
+.card-body {
+  background-color: #ddd;
+}
+
+.chat-body {
+  margin-top: -15px;
+  margin-bottom: -5px;
+  height: 380px;
+  overflow-y: auto;
+}
+
+.speech-bubble {
+  display: inline-block;
+  position: relative;
+  border-radius: 0.4em;
+  padding: 10px;
+  background-color: #fff;
+  font-size: 14px;
+}
+
+.subtle-blue-gradient {
+  background: linear-gradient(45deg,#004bff, #007bff);
+}
+
+.speech-bubble-user:after {
+  content: "";
+  position: absolute;
+  right: 4px;
+  top: 10px;
+  width: 0;
+  height: 0;
+  border: 20px solid transparent;
+  border-left-color: #007bff;
+  border-right: 0;
+  border-top: 0;
+  margin-top: -10px;
+  margin-right: -20px;
+}
+
+.speech-bubble-peer:after {
+  content: "";
+  position: absolute;
+  left: 3px;
+  top: 10px;
+  width: 0;
+  height: 0;
+  border: 20px solid transparent;
+  border-right-color: #ffffff;
+  border-top: 0;
+  border-left: 0;
+  margin-top: -10px;
+  margin-left: -20px;
+}
+
+.chat-section:first-child {
+  margin-top: 10px;
+}
+
+.chat-section {
+  margin-top: 15px;
+}
+
+.send-section {
+  margin-bottom: -20px;
+  padding-bottom: 10px;
+}
+</style>
